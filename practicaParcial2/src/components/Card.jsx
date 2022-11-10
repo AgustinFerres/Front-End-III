b535ebdc1e5f0e2839273db104c27a3c41cb7bae
AgustinFerres <agustinferres4403@gmail.com>
2022-11-10 00:39:38 -0300

    ´fixes´

--- practicaParcial2/src/components/Card.jsx
+++ practicaParcial2/src/components/Card.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-const Card = ({id, title, description, likes, handleEvent}) => {
+const Card = ({title, description, likes, handleEvent}) => {
 
     const [countLikes, setCountLikes] = useState(likes)
 
@@ -13,13 +13,13 @@ const Card = ({id, title, description, likes, handleEvent}) => {
     }
 
     return (
-        <div>
-        <h3>{title}</h3>
-        <p>{description}</p>
-        <div>
-            <button id={id} onClick={handleLikes}>👍</button> <span>{countLikes >= 10 ? `Mas de 10 likes` : `Likes: ${countLikes}`}</span>
-        </div>
-    </div>
+        <article>
+            <h3>{title}</h3>
+            <p>{description}</p>
+            <div>
+                <button onClick={handleLikes}>👍</button> <span>{countLikes >= 10 ? `Mas de 10 likes` : `Likes: ${countLikes}`}</span>
+            </div>
+        </article>
     )
 }
 
